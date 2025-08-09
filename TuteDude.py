96528d52466f7ab732fee002fec8068063396af2
Ashish Singh <indiajj00653@gmail.com>
2025-08-09 16:46:51 +0530

    First all Task

---
+++ TuteDude.py
@@ -0,0 +1,20 @@
+#Task 1
+num = int(input("Enter first number : "))
+num1 = int (input("Enter second number : "))
+
+add = num+num1
+sub = num-num1
+mul = num*num1
+div = num/num1
+
+print("Addition is : ",add)
+print("Subtraction is : ",sub)
+print("Multiplication is : ",mul)
+print("Division is : ",div)
+
+
+#task 2
+user=input("Enter First Name : ")
+user1=input(" Enter Last Name : ")
+
+print(f"Hello {user} {user1} Welcome To The Python Program")
