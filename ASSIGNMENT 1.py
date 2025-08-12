2739d6908906491686314a036463176671a3f80c
Ashish Singh <indiajj00653@gmail.com>
2025-08-13 00:16:25 +0530

    Add files via upload

---
+++ ASSIGNMENT 1.py
@@ -0,0 +1,20 @@
+#Task 1
+user = int(input("Enter first number : "))
+user1 = int (input("Enter second number : "))
+
+add = user+user1
+sub = user-user1
+mul = user*user1
+div = user/user1
+
+print("Addition is : ",add)
+print("Subtraction is : ",sub)
+print("Multiplication is : ",mul)
+print("Division is : ",div)
+
+
+#task 2
+user=input("Enter First Name : ")
+user1=input(" Enter Last Name : ")
+
+print(f"Hello {user} {user1} Welcome To The Python Program")
