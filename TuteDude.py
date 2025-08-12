1f11c1f0b2a8fb37f50f1724dbb0f70652303a4f
Ashish Singh <indiajj00653@gmail.com>
2025-08-12 20:11:56 +0530

    Update TuteDude.py

--- TuteDude.py
+++ TuteDude.py
@@ -1,11 +1,11 @@
 #Task 1
-num = int(input("Enter first number : "))
-num1 = int (input("Enter second number : "))
+user = int(input("Enter first number : "))
+user1 = int (input("Enter second number : "))
 
-add = num+num1
-sub = num-num1
-mul = num*num1
-div = num/num1
+add = user+user1
+sub = user-user1
+mul = user*user1
+div = user/user1
 
 print("Addition is : ",add)
 print("Subtraction is : ",sub)
@@ -16,5 +16,6 @@ print("Division is : ",div)
 #task 2
 user=input("Enter First Name : ")
 user1=input(" Enter Last Name : ")
+
 
 print(f"Hello {user} {user1} Welcome To The Python Program")
